4e13989f69730b725231c384e38d37411879f8d5
ravikant <developer.paudel@gmail.com>
2025-03-09 22:57:47 +0545

    :sparkles: Add MyApp widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pixabay/feature/search/search_page.dart';
 
+import 'feature/favourite/favorite_screen.dart';
+import 'feature/favourite/favourite_bloc.dart';
+import 'feature/search/search_bloc.dart';
+import 'feature/search/search_repository.dart';
+
 void main() {
-  runApp(
-    MaterialApp(
-      home: const SearchPage(),
-    ),
-  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => SearchBloc(SearchRepository()),
+        ),
+        BlocProvider(
+          create: (context) => FavoriteBloc(),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'Pixabay Search',
+        home: const SearchPage(),
+        routes: {
+          '/favorites': (context) => const FavoriteScreen(),
+        },
+      ),
+    );
+  }
 }
